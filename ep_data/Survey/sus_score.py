4dce1d369828dcba6b1052a1861507b8114ed761
chiahou.shen <shenchiahou@MaevaShen.local>
2020-03-10 00:26:13 +0800

    survey updated

--- ep_data/Survey/sus_score.py
+++ ep_data/Survey/sus_score.py
@@ -1,13 +1,35 @@
 import pandas as pd 
-import statistics
+import statistics as st 
+
+
+'''Clean Data'''
 
 data = pd.read_csv('game_sus.csv', sep=",", header=0, encoding="utf-8")
 data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
-data = data.iloc[:,0:12]
+data = data.iloc[:,2:12]
 
-data_qs = {}
+'''Convert negative questions'''
 
-for index, value in data.iterrows():
-    data_qs[value].append(value[index])
 
-print(data_qs)
+
+'''Calculate SUS'''
+
+means = []
+
+for label, content in data.items():
+	content = pd.to_numeric(content, downcast="float")
+	means.append(st.mean(content))
+
+y = 0
+for x in means:
+	x = x*2.5
+	y += x
+
+print(y)
+
+
+'''Cronbach's Alpha'''
+
+
+
+
